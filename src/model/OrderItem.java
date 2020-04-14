2789db0a516f431c9ccd9463b580fef2be640a0f
elkhoukh-soumia <elkhoukhsoumia@gmail.com>
2020-04-14 21:47:07 +0200

    orderItems & Review

--- src/model/OrderItem.java
+++ src/model/OrderItem.java
@@ -3,6 +3,30 @@ package model;
 public class OrderItem {
 	
 	private int id;
-	private int quantite;
+	private int quantity;
+	private Product product;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
 }
