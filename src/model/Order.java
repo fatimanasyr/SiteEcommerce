dfc08ab13b98a0ceefe8b9f74fa6edd95146f0fe
fatimanasyr <fatimanasyr15@gmail.com>
2020-04-14 20:08:23 +0100

    la tache

--- src/model/Order.java
+++ src/model/Order.java
@@ -6,5 +6,35 @@ public class Order {
 	
 	private int id;
 	private Date DateOrder;
+	private OrderItem orderItem;
 
+	public Order(int id, Date dateOrder, OrderItem orderItem) {
+		this.id = id;
+		DateOrder = dateOrder;
+		this.orderItem = orderItem;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDateOrder() {
+		return DateOrder;
+	}
+
+	public void setDateOrder(Date dateOrder) {
+		DateOrder = dateOrder;
+	}
+
+	public OrderItem getOrderItem() {
+		return orderItem;
+	}
+
+	public void setOrderItem(OrderItem orderItem) {
+		this.orderItem = orderItem;
+	}
 }
