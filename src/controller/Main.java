9279b063e7ba5925a30dd33632665b4fa9d6d243
mohamed oubenma <moubenma@gmail.com>
2020-04-14 21:43:33 +0100

    generate aother setters and getters

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -7,11 +7,7 @@ import model.Product;
 public class Main {
 
 	public static void main(String[] args) {
-		Product p1 = new Product(1,"souris",40,60,"","cette souris perment de faire double click",15);
-		Customer c1 = new Customer();
-		Categorie cat1 = new Categorie();
 
-		p1.setCategorie(cat1);
 	}
 
 }
