6433b8ac9e2fe1ffd49336885349c7d1181c0388
elkhoukh-soumia <elkhoukhsoumia@gmail.com>
2020-04-14 22:23:26 +0200

    orderitemes review

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.List;
+
 public class Customer {
 
 	private int id;
@@ -8,5 +10,72 @@ public class Customer {
 	private String email;
 	private int phoneNumber;
 	private String password;
-	
+	private List<Order> listOrder;
+
+
+	public Customer(int id, String firstName, String lastName, String email, int phoneNumber, String password, List<Order> listOrder) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.password = password;
+		this.listOrder = listOrder;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Order> getListOrder() {
+		return listOrder;
+	}
+
+	public void setListOrder(List<Order> listOrder) {
+		this.listOrder = listOrder;
+	}
 }
--- src/model/Order.java
+++ src/model/Order.java
@@ -1,10 +1,42 @@
 package model;
 
 import java.sql.Date;
+import java.util.List;
 
 public class Order {
 	
 	private int id;
 	private Date DateOrder;
+	private OrderItem orderItem;
+	private List<OrderItem> listOrderItem;
 
+	public Order(int id, List<OrderItem> listOrderItem, Date dateOrder) {
+		this.id = id;
+		DateOrder = dateOrder;
+		this.listOrderItem = listOrderItem;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDateOrder() {
+		return DateOrder;
+	}
+
+	public void setDateOrder(Date dateOrder) {
+		DateOrder = dateOrder;
+	}
+
+	public List<OrderItem> getListOrderItem() {
+		return listOrderItem;
+	}
+
+	public void setListOrderItem(List<OrderItem> listOrderItem) {
+		this.listOrderItem = listOrderItem;
+	}
 }
